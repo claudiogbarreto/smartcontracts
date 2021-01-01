681a3b101b621012a3acb7188c2df596a1e6be0c
claudiogbarreto <claudiogbarreto@gmail.com>
2021-01-01 20:12:46 -0300

    Add files via upload

---
+++ olamundo.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: UNLICENSED
+// Specifies the version of Solidity, using semantic versioning.
+// Learn more: https://solidity.readthedocs.io/en/v0.5.10/layout-of-source-files.html#pragma
+pragma solidity 0.6.11;
+
+// Defines a contract named `HelloWorld`.
+// A contract is a collection of functions and data (its state).
+// Once deployed, a contract resides at a specific address on the Ethereum blockchain.
+// Learn more: https://solidity.readthedocs.io/en/v0.5.10/structure-of-a-contract.html
+contract HelloWorld2 {
+
+    // Declares a state variable `message` of type `string`.
+    // State variables are variables whose values are permanently stored in contract storage.
+    // The keyword `public` makes variables accessible from outside a contract
+    // and creates a function that other contracts or clients can call to access the value.
+    string  public message;
+    address public owner = 0x389FCf4677912eAbC301C4D010119854159dd984;
+
+    // Similar to many class-based object-oriented languages, a constructor is
+    // a special function that is only executed upon contract creation.
+    // Constructors are used to initialize the contract's data.
+    // Learn more: https://solidity.readthedocs.io/en/v0.5.10/contracts.html#constructors
+    constructor(string memory _initMessage) public {
+        // Accepts a string argument `initMessage` and sets the value
+        // into the contract's `message` storage variable).
+        message = _initMessage;
+    }
+
+    // A public function that accepts a string argument
+    // and updates the `message` storage variable.
+    function update(string memory _newMessage) public payable {
+        require(owner == msg.sender, 'Apenas o proprietário pode atualizar a mensagem');
+        message = _newMessage;
+    }
+    
+    // A public function that returns the `message` storage variable.
+    function consulta() public view returns (string memory) {
+        return message;
+        
+    }
+    
+}
